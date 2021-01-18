588b2a289de154df4edd47210b132f20c9d32dd3
Kadzitsu <grimm.morev@mail.ru>
2021-01-18 17:00:10 +0300

    Экзамен

---
+++ Возведение числа в степень/Htt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Возведение_числа_в_степень
+{
+    public class Program
+    {
+        private static double a { get; set; }
+        private static double b { get; set; }
+        private static double c { get; set; }
+        static void Main()
+        {
+            Console.WriteLine("Введите число, которое нужно возвести в степень:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите степень, в которую нужно возвести число " + a + ":");
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Math.Pow(a, b);
+            Console.WriteLine(+a + " в степени " + b + " = " + c + "");
+            Console.ReadLine();
+        }
+    }
+}
